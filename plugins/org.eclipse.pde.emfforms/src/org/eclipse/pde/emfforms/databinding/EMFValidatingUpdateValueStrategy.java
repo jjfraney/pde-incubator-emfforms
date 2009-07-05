93e5851757a0bfeaddda230da1328ad7739fce4b
bcabe <bcabe>
2009-07-05 16:59:54 +0000

    the adapterfactory used for message formatting purpose can be static

--- plugins/org.eclipse.pde.emfforms/src/org/eclipse/pde/emfforms/databinding/EMFValidatingUpdateValueStrategy.java
+++ plugins/org.eclipse.pde.emfforms/src/org/eclipse/pde/emfforms/databinding/EMFValidatingUpdateValueStrategy.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     Anyware Technologies - initial API and implementation
  *
- * $Id: EMFValidatingUpdateValueStrategy.java,v 1.4 2009/06/28 09:18:59 bcabe Exp $
+ * $Id: EMFValidatingUpdateValueStrategy.java,v 1.5 2009/07/05 16:59:54 bcabe Exp $
  */
 package org.eclipse.pde.emfforms.databinding;
 
@@ -34,10 +34,9 @@ public class EMFValidatingUpdateValueStrategy extends EMFUpdateValueStrategy {
 
 	public EMFValidatingUpdateValueStrategy() {
 		super();
-		_adapterFactory = new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
 	}
 
-	private ComposedAdapterFactory _adapterFactory;
+	private static ComposedAdapterFactory _adapterFactory = new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
 
 	@Override
 	protected IStatus doSet(IObservableValue observableValue, Object value) {
