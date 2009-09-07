8ab46a9c1a53b04ff316fc6d303bd62708fafc42
bcabe <bcabe>
2009-09-07 13:16:08 +0000

    add non-nls comments

--- plugins/org.eclipse.pde.emfforms/src/org/eclipse/pde/emfforms/internal/editor/IEmfFormsImages.java
+++ plugins/org.eclipse.pde.emfforms/src/org/eclipse/pde/emfforms/internal/editor/IEmfFormsImages.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     Anyware Technologies - initial API and implementation
  *
- * $Id: IEmfFormsImages.java,v 1.3 2009/08/07 11:00:25 bcabe Exp $
+ * $Id: IEmfFormsImages.java,v 1.4 2009/09/07 13:16:08 bcabe Exp $
  */
 
 package org.eclipse.pde.emfforms.internal.editor;
@@ -17,17 +17,17 @@ package org.eclipse.pde.emfforms.internal.editor;
  * Interface used to stock information about images used in the platform editor
  */
 public interface IEmfFormsImages {
-	public static final String ICONS_FOLDER = "icons/";
+	public static final String ICONS_FOLDER = "icons/"; //$NON-NLS-1$
 
-	public static final String DECORATORS_FOLDER = ICONS_FOLDER + "ovr16/";
+	public static final String DECORATORS_FOLDER = ICONS_FOLDER + "ovr16/"; //$NON-NLS-1$
 
-	public static final String ERROR_DECORATOR = DECORATORS_FOLDER + "error.gif";
+	public static final String ERROR_DECORATOR = DECORATORS_FOLDER + "error.gif"; //$NON-NLS-1$
 
-	public static final String WARNING_DECORATOR = DECORATORS_FOLDER + "warning.gif";
+	public static final String WARNING_DECORATOR = DECORATORS_FOLDER + "warning.gif"; //$NON-NLS-1$
 
-	public static final String OBJECT_FOLDER = ICONS_FOLDER + "obj16/";
+	public static final String OBJECT_FOLDER = ICONS_FOLDER + "obj16/"; //$NON-NLS-1$
 
-	public static final String ADD_TOOLBAR_BUTTON = OBJECT_FOLDER + "add.png";
+	public static final String ADD_TOOLBAR_BUTTON = OBJECT_FOLDER + "add.png"; //$NON-NLS-1$
 
-	public static final String REMOVE_TOOLBAR_BUTTON = OBJECT_FOLDER + "remove.png";
+	public static final String REMOVE_TOOLBAR_BUTTON = OBJECT_FOLDER + "remove.png"; //$NON-NLS-1$
 }
